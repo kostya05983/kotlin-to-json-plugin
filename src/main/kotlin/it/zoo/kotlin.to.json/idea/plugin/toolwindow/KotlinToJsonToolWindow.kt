198e7d6815a39cf018b2570cf04224e4d405238a
kostya05983 <kostya05983@mail.ru>
2021-09-18 23:07:46 +0700

    Fixes

--- src/main/kotlin/it/zoo/kotlin.to.json/idea/plugin/toolwindow/KotlinToJsonToolWindow.kt
+++ src/main/kotlin/it/zoo/kotlin.to.json/idea/plugin/toolwindow/KotlinToJsonToolWindow.kt
@@ -1,12 +1,16 @@
 package it.zoo.kotlin.to.json.idea.plugin.toolwindow
 
+import javax.swing.JButton
 import javax.swing.JPanel
 import javax.swing.JTextArea
+import javax.swing.JTextField
 
 class KotlinToJsonToolWindow {
-    private var mainToolWindow: JPanel = JPanel()
+    private lateinit var mainToolWindow: JPanel
 
     private lateinit var kotlinDataClassInput: JTextArea
+    private lateinit var convertButton: JButton
+    private lateinit var text: JTextField
 
     init {
 //        val textArea = JBTextArea("FFFF")
