0247bf366e500774c04357d68246e58ecfad137e
kostya05983 <kostya05983@mail.ru>
2021-09-18 22:33:18 +0700

    Some code

--- src/main/kotlin/it/zoo/kotlin.to.json/idea/plugin/toolwindow/KotlinToJsonToolWindow.kt
+++ src/main/kotlin/it/zoo/kotlin.to.json/idea/plugin/toolwindow/KotlinToJsonToolWindow.kt
@@ -1,9 +1,17 @@
 package it.zoo.kotlin.to.json.idea.plugin.toolwindow
 
 import javax.swing.JPanel
+import javax.swing.JTextArea
 
 class KotlinToJsonToolWindow {
-    private lateinit var mainToolWindow: JPanel
+    private var mainToolWindow: JPanel = JPanel()
+
+    private lateinit var kotlinDataClassInput: JTextArea
+
+    init {
+//        val textArea = JBTextArea("FFFF")
+//        mainToolWindow.add(textArea)
+    }
 
     fun getContent(): JPanel {
         return mainToolWindow
